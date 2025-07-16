254689d354cf9cf8e9805c4fcd1bda56fcb87afc
Rinat <rinatgimaev@gmail.com>
2025-07-16 13:49:22 +0700

    Updated

--- server/src/server.js
+++ server/src/server.js
@@ -6,4 +6,4 @@ app.get("/", (req, res) => {
   res.send("Hi theredd");
 });
 
-app.listen("3001", () => console.log("Listening on server"));
+app.listen("3000", () => console.log("Listening on server 3000"));
